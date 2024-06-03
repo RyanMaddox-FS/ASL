b6536fdc21ebb15bc5342b7bea9be9bee8029840
MaddoxRyan-FS <RAMaddox@student.fullsail.edu>
2024-06-02 23:40:18 -0500

    update migration

--- migrations/20240603040827-create-planet-star.js
+++ migrations/20240603040827-create-planet-star.js
@@ -3,26 +3,12 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('PlanetStars', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
       PlanetId: {
         type: Sequelize.INTEGER
       },
       StarId: {
         type: Sequelize.INTEGER
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
     });
   },
   async down(queryInterface, Sequelize) {
